6d6105b2080c841ae7f00a8fabc1c88b9bc42964
Vasiliy-OVP <62216473+rootok-ovp@users.noreply.github.com>
2022-08-05 18:35:02 +0300

    Update vars.tf

--- infrastructure/vars.tf
+++ infrastructure/vars.tf
@@ -1,7 +1,7 @@
 variable "access_key" {
-  default = "AKIAS67HTA4BITXGSDMY"
+  default = "AK"
 }
 
 variable "secret_key" {
-    default = "Q2y9y4pCb/ro8tjknEeETojaV168CHo/jEMp9JWi"
+    default = "Q2y9i"
 }
