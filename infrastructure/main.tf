a127ebd61529cc1499d22e2bfb9e67cf374182a3
Vasiliy-OVP <62216473+rootok-ovp@users.noreply.github.com>
2022-08-08 22:33:44 +0300

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "Devops1-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
