3c9d608f6c2e2f5aa00de3944e9c4b593bc59915
Vasiliy-OVP <62216473+rootok-ovp@users.noreply.github.com>
2022-08-09 18:55:18 +0300

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,9 @@
+cloud {
+    organization = "sabotage-ivet"
+    workspaces {
+      name = "devops_ovp"
+    }
+  }
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
