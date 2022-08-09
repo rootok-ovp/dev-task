0e7f71791884eca6d17e33d4c48306a25105db07
Vasiliy-OVP <62216473+rootok-ovp@users.noreply.github.com>
2022-08-09 20:11:20 +0300

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,12 +5,6 @@ terraform {
               name = "devops1"
             }
         }
-        required_providers {
-            aws = {
-              source = "hashicorp/aws"
-            }
-        }
-        required_version = ">= 1.2.0"
 }
 
 provider "kubernetes" {
