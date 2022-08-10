1ca24d246fcafa9224f37de82e708bcc202a5708
Vasiliy-OVP <62216473+rootok-ovp@users.noreply.github.com>
2022-08-10 12:33:49 +0300

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,9 +18,6 @@ provider "aws" {
 
 data "aws_available_zones" "available" {}
 
-locals {
-  cluster_name = "Devops1-eks-${random_string.suffix.result}"
-}
 
 resource "random_string" "suffix" {
   length  = 8
