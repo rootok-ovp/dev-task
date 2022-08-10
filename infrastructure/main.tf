cfb2fec06868867787143557758d26594648f24d
Vasiliy-OVP <62216473+rootok-ovp@users.noreply.github.com>
2022-08-10 14:46:48 +0300

    Add files via upload

---
+++ infrastructure/main.tf
@@ -0,0 +1,28 @@
+terraform {
+        cloud {
+            organization = "devops_ovp"
+            workspaces {
+              name = "devops1"
+            }
+        }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
+provider "aws" {
+  region     = "us-west-2"
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  cluster_name = "education-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
