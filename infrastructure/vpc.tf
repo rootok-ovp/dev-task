454fa0e1947a491a0412466be5aaf2aa1ffffbac
Vasiliy-OVP <62216473+rootok-ovp@users.noreply.github.com>
2022-08-10 15:20:39 +0300

    Update vpc.tf

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -18,7 +18,7 @@ module vpc {
     public_subnets =  ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
     enable_nat_gateway = true
     single_nat_gateway = true
-    enable_dns_hostname = true
+    enable_dns_hostnames = true
 
 tags = {
     "Name" = "Demo-VPC"
