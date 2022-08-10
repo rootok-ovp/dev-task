58ef9c7a9ca1dc8239cdc6b0c050a917be53f5f6
Vasiliy-OVP <62216473+rootok-ovp@users.noreply.github.com>
2022-08-10 14:26:18 +0300

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -21,8 +21,3 @@ data "aws_availability_zones" "available" {}
 locals {
   cluster_name = "Devops1-EKS-${random_string.suffix.result}"
 }
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
