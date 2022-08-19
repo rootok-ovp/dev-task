b2e84e1c5e07ffeb316c01ea15355b899d97b758
Vasiliy-OVP <62216473+rootok-ovp@users.noreply.github.com>
2022-08-19 14:12:56 +0300

    Update security.tf

--- infrastructure/security.tf
+++ infrastructure/security.tf
@@ -2,70 +2,109 @@ resource "aws_security_group" "worker_group_mgmt_one" {
     name_prefix = "worker_group_mgmt_one"
     vpc_id = module.vpc.vpc_id
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-        cidr_blocks = [
-            "10.0.0.0/8"
-        ]
-    }
-        ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-
-        cidr_blocks = [
-            "10.0.0.0/8"
-        ]
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0" ]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0" ]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0" ]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0" ]
+  }
 }
 
 resource "aws_security_group" "worker_group_mgmt_two" {
     name_prefix = "worker_group_mgmt_two"
     vpc_id = module.vpc.vpc_id
  
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-        cidr_blocks = [
-            "10.0.0.0/8"
-        ]
-    }
-        ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-
-        cidr_blocks = [
-            "10.0.0.0/8"
-        ]
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0" ]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0" ]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0" ]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0" ]
+  }
 }
 
 resource "aws_security_group" "all_worker_mgmt" {
     name_prefix = "all_worker_management"
     vpc_id = module.vpc.vpc_id
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-        cidr_blocks = [
-            "10.0.0.0/8"
-        ]
-    }
-            ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-
-        cidr_blocks = [
-            "10.0.0.0/8"
-        ]
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0" ]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0" ]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0" ]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0" ]
+  }
 }
